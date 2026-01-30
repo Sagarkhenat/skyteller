4d1d45142377ed06aeca7ebbc3436367eaf59d35
unknown <sgkhenat94@gmail.com>
2026-01-30 19:00:46 +0530

    pipe for temp celsius/fahrenheit added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Nav, Platform, Events, AlertController, IonicApp ,MenuController} from 'ionic-angular';
+import { App } from 'ionic-angular/components/app/app';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +10,15 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(public platform: Platform) {
+
+    this.platform.ready().then(() => {
+
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+
+    });
+
+
+  }
 }
